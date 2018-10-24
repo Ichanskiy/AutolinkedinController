aa679268b3f2cb42fc667fc9c649ee181913b7ba
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-10-24 16:39:35 +0300

    remove Admin Server

--- src/main/java/tech/mangosoft/autolinkedin/AutolinkedinControllerApplication.java
+++ src/main/java/tech/mangosoft/autolinkedin/AutolinkedinControllerApplication.java
@@ -1,11 +1,9 @@
 package tech.mangosoft.autolinkedin;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
 public class AutolinkedinControllerApplication {
 
     public static void main(String[] args) {
