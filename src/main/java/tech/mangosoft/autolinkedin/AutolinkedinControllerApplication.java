d37370cc575b803a2536133f647c8eb06874c005
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-10-25 10:15:55 +0300

    update spring version and AutolinkedinControllerApplication

--- src/main/java/tech/mangosoft/autolinkedin/AutolinkedinControllerApplication.java
+++ src/main/java/tech/mangosoft/autolinkedin/AutolinkedinControllerApplication.java
@@ -2,9 +2,10 @@ package tech.mangosoft.autolinkedin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AutolinkedinControllerApplication {
+public class AutolinkedinControllerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AutolinkedinControllerApplication.class, args);
