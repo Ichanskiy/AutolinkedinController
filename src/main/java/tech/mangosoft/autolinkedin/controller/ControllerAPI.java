b85c3523397de368f89d90ad9f2a9edbe1a859ea
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-11-07 17:02:59 +0200

    update ControllerAPI

--- src/main/java/tech/mangosoft/autolinkedin/controller/ControllerAPI.java
+++ src/main/java/tech/mangosoft/autolinkedin/controller/ControllerAPI.java
@@ -5,6 +5,9 @@ public class ControllerAPI{
     static final String ACCOUNT_CONTROLLER = "/account";
     static final String BY_PASSWORD = "/password";
 
+    static final String ADMIN_CONTROLLER = "/admin";
+    static final String ALL_BY_PAGE = "/all/{page}";
+
     static final String BY_ID = "/{id}";
 
 }
