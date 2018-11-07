3ca5f0bef7883f3a156cc71b1b50d0204f67c198
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-11-07 17:32:36 +0200

    fix AccountControllerTest

--- src/test/java/tech/mangosoft/autolinkedin/controller/AccountControllerTest.java
+++ src/test/java/tech/mangosoft/autolinkedin/controller/AccountControllerTest.java
@@ -1,20 +1,24 @@
 package tech.mangosoft.autolinkedin.controller;
 
+import org.apache.logging.log4j.util.Strings;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import tech.mangosoft.autolinkedin.db.entity.Account;
 import tech.mangosoft.autolinkedin.db.repository.IAccountRepository;
 import tech.mangosoft.autolinkedin.service.AccountService;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.*;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -41,18 +45,29 @@ public class AccountControllerTest {
     @Test
     public void getAccountValid() throws Exception {
         String request = ControllerAPI.ACCOUNT_CONTROLLER + ControllerAPI.BY_ID;
-        Account account = new Account("first", "last", "username", "pas", 100);
-        account.setId(10L);
-        when(accountRepository.getById(account.getId())).thenReturn(account);
-        mockMvc.perform(get(request, account.getId())).andExpect(status().isOk()).andReturn();
-//        verifyNoMoreInteractions(accountRepository);
+        when(accountRepository.getById(1L)).thenReturn(new Account());
+        MockHttpServletResponse response = mockMvc
+                .perform(get(request, 1L))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+        assertNotEquals(response.getContentAsString(), Strings.EMPTY);
+        verify(accountRepository, times(1)).getById(1L);
+        verifyNoMoreInteractions(accountRepository);
     }
 
     @Test
     public void getAccountInvalid() throws Exception {
         String request = ControllerAPI.ACCOUNT_CONTROLLER + ControllerAPI.BY_ID;
-        when(accountRepository.getById(10L)).thenReturn(Mockito.any(Account.class));
-        mockMvc.perform(get(request, 10L)).andExpect(status().isBadRequest()).andReturn();
+        when(accountRepository.getById(1L)).thenReturn(null);
+        MockHttpServletResponse response = mockMvc
+                .perform(get(request, 1L))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResponse();
+        assertEquals(response.getContentAsString(), Strings.EMPTY);
+        verify(accountRepository, times(1)).getById(1L);
+        verifyNoMoreInteractions(accountRepository);
     }
 
 }
