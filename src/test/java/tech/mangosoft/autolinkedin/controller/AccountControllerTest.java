27b8704fd2fb8c6d391191ab877780d78a071cdf
VestDev <vestdev@mangosoft.tech>
2018-11-08 13:24:44 +0200

    tests

--- src/test/java/tech/mangosoft/autolinkedin/controller/AccountControllerTest.java
+++ src/test/java/tech/mangosoft/autolinkedin/controller/AccountControllerTest.java
@@ -8,10 +8,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import tech.mangosoft.autolinkedin.db.entity.Account;
+import tech.mangosoft.autolinkedin.db.entity.Company;
+import tech.mangosoft.autolinkedin.db.entity.enums.Role;
 import tech.mangosoft.autolinkedin.db.repository.IAccountRepository;
 import tech.mangosoft.autolinkedin.service.AccountService;
 
@@ -20,11 +23,17 @@ import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Mockito.*;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static tech.mangosoft.autolinkedin.utils.JacksonUtils.getJson;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AccountControllerTest {
+    private static final String EMAIL = "test@email.com";
+    private static final String PASSWORD = "qwe123";
+    private static final String FIRST = "first";
+    private static final String LAST = "last";
+    private static final String COMPANY = "TestSoft";
 
     private MockMvc mockMvc;
 
@@ -36,7 +45,6 @@ public class AccountControllerTest {
 
     @InjectMocks
     private AccountController accountController;
-
     @Before
     public void before() {
         MockitoAnnotations.initMocks(this);
@@ -46,37 +54,31 @@ public class AccountControllerTest {
     @Test
     public void getAccountValid() throws Exception {
         String request = ControllerAPI.ACCOUNT_CONTROLLER;
-        Account account = new Account();
-        account.setUsername("test@email.com");
-        account.setPassword("qwe123");
-        when(accountRepository.getAccountByUsernameAndPassword(account.getUsername(), account.getPassword()))
+        when(accountRepository.getAccountByUsernameAndPassword(EMAIL, PASSWORD))
                 .thenReturn(new Account());
         MockHttpServletResponse response = mockMvc
-                .perform(get(request))
+                .perform(get(request).param("login", EMAIL).param("password", PASSWORD))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse();
         assertNotEquals(response.getContentAsString(), Strings.EMPTY);
         verify(accountRepository, times(1))
-                .getAccountByUsernameAndPassword(account.getUsername(), account.getPassword());
+                .getAccountByUsernameAndPassword(EMAIL, PASSWORD);
         verifyNoMoreInteractions(accountRepository);
     }
 
     @Test
     public void getAccountInvalid() throws Exception {
         String request = ControllerAPI.ACCOUNT_CONTROLLER;
-        Account account = new Account();
-        account.setUsername("test@email.com");
-        account.setPassword("qwe123");
-        when(accountRepository.getAccountByUsernameAndPassword(account.getUsername(), account.getPassword())).thenReturn(null);
+        when(accountRepository.getAccountByUsernameAndPassword(EMAIL, PASSWORD)).thenReturn(null);
         MockHttpServletResponse response = mockMvc
-                .perform(get(request, account.getUsername(), account.getPassword()))
+                .perform(get(request).param("login", EMAIL).param("password", PASSWORD))
                 .andExpect(status().isBadRequest())
                 .andReturn()
                 .getResponse();
         assertEquals(response.getContentAsString(), Strings.EMPTY);
         verify(accountRepository, times(1))
-                .getAccountByUsernameAndPassword(account.getUsername(), account.getPassword());
+                .getAccountByUsernameAndPassword(EMAIL, PASSWORD);
         verifyNoMoreInteractions(accountRepository);
     }
 
@@ -107,5 +109,31 @@ public class AccountControllerTest {
         verify(accountRepository, times(1)).getById(1L);
         verifyNoMoreInteractions(accountRepository);
     }
+
+    @Test
+    public void createAccountValid() throws Exception {
+        String request = ControllerAPI.ACCOUNT_CONTROLLER;
+        Account account = new Account();
+        account.setUsername(EMAIL);
+        account.setPassword(PASSWORD);
+        account.setFirst(FIRST);
+        account.setLast(LAST);
+        account.setGrabbingLimit(100);
+        Company company = new Company().setName(COMPANY);
+        account.setCompany(company);
+        when(accountService.accountNotValid(account)).thenReturn(false);
+        when(accountService.createAccount(account)).thenReturn(account);
+        MockHttpServletResponse response = mockMvc
+                .perform(post(request).content(getJson(account))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+        assertNotEquals(response.getContentAsString(), Strings.EMPTY);
+        verify(accountService, times(1)).accountNotValid(account);
+        verify(accountService, times(1)).createAccount(account);
+        verifyNoMoreInteractions(accountService);
+    }
+
 
 }
