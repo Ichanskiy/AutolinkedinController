ffbb1a87e62456fe883700deca395764af11d78f
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-11-27 14:53:18 +0200

    update DeleteContactServices

--- src/main/java/tech/mangosoft/autolinkedin/service/DeleteContactServices.java
+++ src/main/java/tech/mangosoft/autolinkedin/service/DeleteContactServices.java
@@ -2,16 +2,17 @@ package tech.mangosoft.autolinkedin.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tech.mangosoft.autolinkedin.controller.messages.ContactsMessage;
 import tech.mangosoft.autolinkedin.db.entity.Assignment;
 import tech.mangosoft.autolinkedin.db.entity.LinkedInContact;
 import tech.mangosoft.autolinkedin.db.repository.IAssignmentRepository;
 import tech.mangosoft.autolinkedin.db.repository.IContactProcessingRepository;
+import tech.mangosoft.autolinkedin.db.repository.ILinkedInContactRepository;
 import tech.mangosoft.autolinkedin.db.repository.ILocationRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -20,6 +21,9 @@ public class DeleteContactServices {
 
     @Autowired
     private ContactService contactService;
+
+    @Autowired
+    private ILinkedInContactRepository linkedInContactRepository;
 
     @Autowired
     private ILocationRepository locationRepository;
@@ -34,15 +38,24 @@ public class DeleteContactServices {
     private EntityManagerFactory emf;
 
     public void testDelete() {
-        ContactsMessage contactsMessage = new ContactsMessage();
-        contactsMessage.setIndustries("Financial Services");
-        contactsMessage.setLocation(locationRepository.getById(1L).getLocation());
-        contactsMessage.setPosition("CTO");
-        contactsMessage.setUserId(2L);
+//        ContactsMessage contactsMessage = new ContactsMessage();
+//        contactsMessage.setIndustries("Financial Services");
+//        contactsMessage.setLocation(locationRepository.getById(1L).getLocation());
+//        contactsMessage.setPosition("CTO");
+//        contactsMessage.setUserId(2L);
 
+        List<LinkedInContact> contacts = linkedInContactRepository.getAllByAssignments(assignmentRepository.getById(1256L));
+        System.out.println(contacts);
         int i = 0;
-        List<LinkedInContact> contacts = contactService.getListContactsByParams(contactsMessage);
+//        List<LinkedInContact> contacts = contactService.getListContactsByParams(contactsMessage);
         for (LinkedInContact contact : contacts) {
+//            List<ContactProcessing> allByAssignmentId = contactProcessingRepository.getAllByAssignmentId(1256L);
+//            for (ContactProcessing contactProcessing : allByAssignmentId) {
+//                delete(contactProcessing);
+//                i++;
+////                delete(contact);
+//                System.out.println("i === " + i);
+//            }
             i++;
             delete(contact);
             System.out.println("i = " + i);
@@ -58,8 +71,12 @@ public class DeleteContactServices {
         LinkedInContact lc = entityManager.find(LinkedInContact.class, contact.getId());
         if (lc.getAssignments() != null) {
             Set<Assignment> assignments = lc.getAssignments();
-            for (Assignment assignment : assignments) {
-                lc.removeAssignment(assignment);
+
+            Iterator<Assignment> iterator = assignments.iterator();
+            while (iterator.hasNext()) {
+                Assignment setElement = iterator.next();
+                iterator.remove();
+                lc.removeAssignment(setElement);
             }
         }
         entityManager.remove(lc);
